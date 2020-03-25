6c98484f077dfb8ff308d33e9542dde1c568a2fb
VergaraC <victorvaac@al.insper.edu.br>
2020-03-25 15:34:31 -0300

    Comparador

--- Projetos/C-UnidadeLogicaAritmetica/src/rtl/comparador16.vhd
+++ Projetos/C-UnidadeLogicaAritmetica/src/rtl/comparador16.vhd
@@ -2,14 +2,14 @@
 -- by Luciano Soares
 -- comparador16.vhd
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
+Library ieee;
+use ieee.std_logic_1164.all;
 
 entity comparador16 is
    port(
-	     a    : in STD_LOGIC_VECTOR(15 downto 0);
-       zr   : out STD_LOGIC;
-       ng   : out STD_LOGIC
+	     a    : in std_logic_vector(15 downto 0);
+       zr   : out std_logic;
+       ng   : out std_logic
    );
 end comparador16;
 
@@ -20,5 +20,13 @@ architecture rtl of comparador16 is
 
 begin
   -- Implementação vem aqui!
+  with a select
+    zr <= '1'when "0000000000000000",
+          '0'  when others;
+    with a(15) select
+    ng <= '1' when'1',
+          '0' when others;
+
+
 
 end architecture;
