b1cdbcb3e427b2e3b7c25a5fa6a7e78869358015
GuilhermeLunetta <lunettagui@gmail.com>
2020-04-06 22:27:34 -0300

    ram512 testado e funcionando

--- Projetos/D-LogicaSequencial/src/rtl/Ram512.vhd
+++ Projetos/D-LogicaSequencial/src/rtl/Ram512.vhd
@@ -22,7 +22,7 @@ architecture arch of Ram512 is
 			clock:   in  STD_LOGIC;
 			input:   in  STD_LOGIC_VECTOR(15 downto 0);
 			load:    in  STD_LOGIC;
-			address: in  STD_LOGIC_VECTOR( 5 downto 0);
+			address: in  STD_LOGIC_VECTOR(5 downto 0);
 			output:  out STD_LOGIC_VECTOR(15 downto 0)
 		);
 	end component;
@@ -60,5 +60,94 @@ architecture arch of Ram512 is
 
 begin
 
+	r0: Ram64 port map(
+		clock,
+		input,
+		load0, 
+		address(5 downto 0),
+		output0
+	);
+	
+	r1: Ram64 port map(
+		clock,
+		input,
+		load1, 
+		address(5 downto 0), 
+		output1
+	);
+	
+	r2: Ram64 port map(
+		clock,
+		input,
+		load2, 
+		address(5 downto 0), 
+		output2
+	);
+
+	r3: Ram64 port map(
+		clock,
+		input,
+		load3, 
+		address(5 downto 0), 
+		output3
+	);
+
+	r4: Ram64 port map(
+		clock,
+		input,
+		load4, 
+		address(5 downto 0), 
+		output4
+	);
+
+	r5: Ram64 port map(
+		clock,
+		input,
+		load5, 
+		address(5 downto 0), 
+		output5
+	);
+
+	r6: Ram64 port map(
+		clock,
+		input,
+		load6, 
+		address(5 downto 0), 
+		output6
+	);
+
+	r7: Ram64 port map(
+		clock,
+		input,
+		load7, 
+		address(5 downto 0), 
+		output7
+	);
+
+	outsMux: Mux8Way16 port map( 
+		output0,
+		output1,
+		output2,
+		output3,
+		output4,
+		output5,
+		output6,
+		output7,
+		address(8 downto 6),
+		output
+	);
+
+	outsDMux: DMux8Way port map( 
+		load,
+		address(8 downto 6),
+		load0,
+		load1,
+		load2,
+		load3,
+		load4,
+		load5,
+		load6,
+		load7
+	); 
 
 end architecture;
