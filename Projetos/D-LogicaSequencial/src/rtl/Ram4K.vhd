015055db4b08a13eed2b8f0607c3e4bbf7d19b3e
borg <antoniovf@al.insper.edu.br>
2020-04-06 22:47:56 -0300

    Ram-4k testada e funcionando

--- Projetos/D-LogicaSequencial/src/rtl/Ram4K.vhd
+++ Projetos/D-LogicaSequencial/src/rtl/Ram4K.vhd
@@ -60,5 +60,114 @@ architecture arch of Ram4K is
 
 begin
 
+	DmuxRam: DMux8Way
+	port map
+	(
+		a => load,
+		sel => address(11 downto 9),
+		q0 => load0,
+		q1 => load1,
+		q2 => load2,
+		q3 => load3,
+		q4 => load4,
+		q5 => load5,
+		q6 => load6,
+		q7 => load7
+	);
+
+	MuxRam: Mux8Way16
+	port map
+	(
+		a => output0,
+		b => output1,
+		c => output2,
+		d => output3,
+		e => output4,
+		f => output5,
+		g => output6,
+		h => output7,
+		sel => address(11 downto 9),
+		q => output
+	);
+
+	Ram4k0: Ram512
+	port map
+	(
+		clock => clock,
+		input => input,
+		load => load0,
+		address => address(8 downto 0),
+		output => output0
+	);
+
+	Ram4k1: Ram512
+	port map
+	(
+		clock => clock,
+		input => input,
+		load => load1,
+		address => address(8 downto 0),
+		output => output1
+	);
+
+	Ram4k2: Ram512
+	port map
+	(
+		clock => clock,
+		input => input,
+		load => load2,
+		address => address(8 downto 0),
+		output => output2
+	);
+
+	Ram4k3: Ram512
+	port map
+	(
+		clock => clock,
+		input => input,
+		load => load3,
+		address => address(8 downto 0),
+		output => output3
+	);
+
+	Ram4k4: Ram512
+	port map
+	(
+		clock => clock,
+		input => input,
+		load => load4,
+		address => address(8 downto 0),
+		output => output4
+	);
+
+	Ram4k5: Ram512
+	port map
+	(
+		clock => clock,
+		input => input,
+		load => load5,
+		address => address(8 downto 0),
+		output => output5
+	);
+
+	Ram4k6: Ram512
+	port map
+	(
+		clock => clock,
+		input => input,
+		load => load6,
+		address => address(8 downto 0),
+		output => output6
+	);
+
+	Ram4k7: Ram512
+	port map
+	(
+		clock => clock,
+		input => input,
+		load => load7,
+		address => address(8 downto 0),
+		output => output7
+	);
 
 end architecture;
