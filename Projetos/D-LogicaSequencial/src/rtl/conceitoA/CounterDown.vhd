6f45b32e55cf3937ad6c3322060116bfafbfc72e
VergaraC <vvergara@uol.com.br>
2020-04-09 23:50:27 -0300

    ora

--- Projetos/D-LogicaSequencial/src/rtl/conceitoA/CounterDown.vhd
+++ Projetos/D-LogicaSequencial/src/rtl/conceitoA/CounterDown.vhd
@@ -7,7 +7,8 @@ use ieee.std_logic_1164.all;
 entity CounterDown is
 	port(
 		clock:  in std_logic;
-		q:      out std_logic_vector(2 downto 0) :=(others => '0')
+		q:      out std_logic_vector(2 downto 0) :=(others => '0');
+		notq:   out std_logic_vector(2 downto 0) :=(others => '0')
 	);
 end entity;
 
@@ -23,8 +24,6 @@ component FlipFlopT is
 	);
 end component;
 
-signal q0, q1, q2: std_logic;
-
 begin
 
 			f0: FlipFlopT port map
@@ -32,7 +31,7 @@ begin
 				clock => clock,
 				t => '1',
 				q => q(0),
-				notq => open
+				notq => notq(0)
 			);
 
 			f1: FlipFlopT port map
@@ -40,7 +39,7 @@ begin
 				clock => q(0),
 				t => '1',
 				q => q(1),
-				notq => open
+				notq => notq(1)
 			);
 
 			f2: FlipFlopT port map
@@ -48,7 +47,7 @@ begin
 				clock => q(1),
 				t => '1',
 				q => q(2),
-				notq => open
+				notq => notq(2)
 			);
 
 end architecture;
