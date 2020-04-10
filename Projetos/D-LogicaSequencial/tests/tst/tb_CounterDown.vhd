90eae2e2c41e64b7aa1cadc2d94dc9ac436c061c
VergaraC <vvergara@uol.com.br>
2020-04-10 00:10:23 -0300

    Desisto

--- Projetos/D-LogicaSequencial/tests/tst/tb_CounterDown.vhd
+++ Projetos/D-LogicaSequencial/tests/tst/tb_CounterDown.vhd
@@ -17,10 +17,11 @@ architecture tb of tb_CounterDown is
     port(
       clock:  in std_logic;
       q:      out std_logic_vector(2 downto 0)
+      
       );
     end component;
 
-    signal clk : std_logic := '0';
+  signal clk : std_logic := '0';
   signal q   : std_logic_vector(2 downto 0);
 
 begin
@@ -35,28 +36,28 @@ begin
 
     -- TESTE 1
     wait until clk'event and clk='0';
-        assert(Q = "111")  report "Falha em teste 1" severity error;
+        assert(q = "111")  report "Falha em teste 1" severity error;
     -- TESTE 2
     wait until clk'event and clk='0';
-        assert(Q = "110")  report "Falha em teste 2" severity error;
+        assert(q = "110")  report "Falha em teste 2" severity error;
     -- TESTE 3
     wait until clk'event and clk='0';
-        assert(Q = "101")  report "Falha em teste 3" severity error;
+        assert(q = "101")  report "Falha em teste 3" severity error;
     -- TESTE 4
     wait until clk'event and clk='0';
-        assert(Q = "100")  report "Falha em teste 4" severity error;
+        assert(q = "100")  report "Falha em teste 4" severity error;
     -- TESTE 5
     wait until clk'event and clk='0';
-        assert(Q = "011")  report "Falha em teste 5" severity error;
+        assert(q = "011")  report "Falha em teste 5" severity error;
     -- TESTE 6
     wait until clk'event and clk='0';
-    assert(Q = "010")  report "Falha em teste 6" severity error;
+        assert(q = "010")  report "Falha em teste 6" severity error;
     -- TESTE 7
     wait until clk'event and clk='0';
-    assert(Q = "001")  report "Falha em teste 7" severity error;
+        assert(q = "001")  report "Falha em teste 7" severity error;
     -- TESTE 8
     wait until clk'event and clk='0';
-    assert(Q = "000")  report "Falha em teste 8" severity error;
+        assert(q = "000")  report "Falha em teste 8" severity error;
 
 
 
